fa8abd673d8b97f7c24b7b819675b9c819c35d84
abhay zulasana <abhayzulasana@gmail.com>
2025-03-26 14:39:19 +0530

    authLayout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sigj in to your account
+          Sign in to your account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
           Don&apos;t have any account?&nbsp;
